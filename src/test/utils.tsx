54b0658d567aecd1414a7877e0cde3364ea4be52
jelly-rtzr <jelly@rtzr.ai>
2025-03-11 02:52:26 +0900

    테스트 유틸리티 파일의 render 함수 문제 해결

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,7 +1,7 @@
 // @ts-ignore React 타입 에러 무시
 import React, { ReactElement } from 'react';
 // @ts-ignore testing-library 타입 에러 무시
-import { render, RenderOptions } from '@testing-library/react';
+import { render as rtlRender, RenderOptions, screen } from '@testing-library/react';
 // @ts-ignore user-event 타입 에러 무시
 import userEvent from '@testing-library/user-event';
 import { ThemeProvider } from 'next-themes';
@@ -22,11 +22,11 @@ const customRender = (
 ) => {
   return {
     user: userEvent.setup(),
-    ...render(ui, { wrapper: AllTheProviders, ...options }),
+    ...rtlRender(ui, { wrapper: AllTheProviders, ...options }),
   };
 };
 
 // 테스트 유틸리티 함수들 재내보내기
 // @ts-ignore testing-library export 에러 무시
 export * from '@testing-library/react';
-export { customRender as render, screen } from '@testing-library/react'; 
+export { customRender as render, screen, userEvent }; 
